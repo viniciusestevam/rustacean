d6f337f302c76aceca8e2d9d20c4a721b5b19d5d
Vinicius Estevam <estevamvinicius31@gmail.com>
2021-11-20 12:43:46 -0300

    chapter-7: packages, crates and modules

---
+++ chapter-7/restaurant/src/lib.rs
@@ -0,0 +1,64 @@
+// mod front_of_house {
+//     mod hosting {
+//         fn add_to_waitlist() {}
+
+//         fn seat_at_table() {}
+//     }
+
+//     mod serving {
+//         fn take_order() {}
+
+//         fn serve_order() {}
+
+//         fn take_payment() {}
+//     }
+// }
+
+// fn serve_order() {}
+
+// mod back_of_house {
+//     fn fix_incorrect_order() {
+//         cook_order();
+//         super::serve_order();
+//     }
+
+//     fn cook_order() {}
+// }
+
+// mod front_of_house {
+//     pub mod hosting {
+//         fn add_to_waitlist() {}
+//     }
+// }
+
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
+
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+// }
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
