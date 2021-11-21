7540dccb0a24436e06341ee144e04df885206ed0
Vinicius Estevam <estevamvinicius31@gmail.com>
2021-11-21 20:23:16 -0300

    chapter-12: reading file contents

--- chapter-12/minigrep/src/main.rs
+++ chapter-12/minigrep/src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
     let filename = &args[2];
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("File content:\n{}", contents);
 }
