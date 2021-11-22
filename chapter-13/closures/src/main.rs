f722c7dc6b4123f15c5ac8e0f67c0aea79dccc03
Vinicius Estevam <estevamvinicius31@gmail.com>
2021-11-21 23:05:05 -0300

    chapter-13: closures

---
+++ chapter-13/closures/src/main.rs
@@ -0,0 +1,69 @@
+use std::cmp::Eq;
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::thread;
+use std::time::Duration;
+
+struct Cacher<T, K, V>
+where
+    T: Fn(K) -> V,
+    K: Eq + Hash + Copy,
+    V: Copy,
+{
+    calculation: T,
+    value: HashMap<K, V>,
+}
+
+impl<T, K, V> Cacher<T, K, V>
+where
+    T: Fn(K) -> V,
+    K: Eq + Hash + Copy,
+    V: Copy,
+{
+    fn new(calculation: T) -> Cacher<T, K, V> {
+        Cacher {
+            calculation,
+            value: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: K) -> V {
+        match self.value.get(&arg) {
+            Some(v) => *v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value.insert(arg, v);
+                v
+            }
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
